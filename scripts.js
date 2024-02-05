562c08c5a4edba5ac4d46e4617f19557c82d11e0
zoofboof <zoofboof@gmail.com>
2024-02-04 20:29:14 -0500

    4

---
+++ scripts.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var project1 = document.getElementById("project1");
+    var project2 = document.getElementById("project2");
+  
+    project1.addEventListener("click", function () {
+      window.location.href = "project1.html"; // Replace with the actual URL for Project 1
+    });
+  
+    project2.addEventListener("click", function () {
+      window.location.href = "project2.html"; // Replace with the actual URL for Project 2
+    });
+  
+    // Add similar event listeners for other projects as needed
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    $('#videoModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget);
+      var videoId = button.data('video-id');
+      var modal = $(this);
+      modal.find('#videoPlayer').attr('src', 'https://www.youtube.com/embed/' + videoId + '?rel=0');
+    });
+  
+    $('#videoModal').on('hide.bs.modal', function () {
+      var modal = $(this);
+      modal.find('#videoPlayer').attr('src', ''); // Pause the video when modal is closed
+    });
+  });
